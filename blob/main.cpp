6dca24849bb7c7a54d8b94b1a4eb51ac269ca6eb
ReemAlattas <ralattas@live.com>
2016-07-15 18:33:14 +0300

    Find top right of blob

--- blob/main.cpp
+++ blob/main.cpp
@@ -7,9 +7,42 @@
 //
 
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
+
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    const int N = 10;
+    int blobCounter[N*N];
+    int top = 0, left = 0, bottom = 0, right = 0;
+    
+    int blob[N][N] = {{0,0,0,0,0,0,0,0,0,0},
+            {0,0,1,1,1,0,0,0,0,0},
+            {0,0,1,1,1,1,1,0,0,0},
+            {0,0,1,0,0,0,1,0,0,0},
+            {0,0,1,1,1,1,1,0,0,0},
+            {0,0,0,0,1,0,1,0,0,0},
+            {0,0,0,0,1,0,1,0,0,0},
+            {0,0,0,0,1,1,1,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0}};
+    
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (blob[i][j]) {
+                top = i;
+                left = j;
+                goto top_left_found;
+            }
+        }
+    }
+    
+top_left_found:
+    cout << "Top = " << top << endl;
+    cout << "Left = " << left << endl;
+    
     return 0;
 }
